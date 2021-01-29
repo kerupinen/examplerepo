98016ca657eb81a657d51ba3de8fad0b16aa2041
Kristen Rupinen <kristenrupinen@Kristens-MacBook-Pro-2.local>
2021-01-28 20:22:09 -0600

    updated welcome message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplerepos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git is great!");
         }
     }
 }
